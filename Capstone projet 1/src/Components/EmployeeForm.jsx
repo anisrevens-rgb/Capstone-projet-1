afc7874b0f83118b1b2da62788f3662b2bf6a79c
Anis Revens <anisrevens@gmail.com>
2026-03-25 14:33:36 -0700

    Realisation du formulaire

--- Capstone projet 1/src/Components/EmployeeForm.jsx
+++ Capstone projet 1/src/Components/EmployeeForm.jsx
@@ -1,4 +1,36 @@
 import "./EmployeeForm.css"
 export default function EmployeeForm(){
+return(
+    <form className="form-container">
+        <div className="form-card">
+            <label htmlFor="name">Nom </label>
+            <input
+            id="name"
+            type="text"
+            placeholder="Entrer le nom"
+            />
+            
+        </div>
+            <div className="form-card">
+            <label htmlFor="position">Position </label>
+            <input
+            id="position"
+            type="text"
+            placeholder="Entrer la position"
+            />
+            
+        </div>
+            <div className="form-card">
+            <label htmlFor="departement">Departement</label>
+            <input
+            id="name"
+            type="text"
+            placeholder="Entrer le departement"
+            />
+            
+        </div>
+        <button type="submit" className="form-button">Ajouter l'employe</button>
 
+    </form>
+)
 }
